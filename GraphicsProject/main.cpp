5b4f082e9b5a819c3b4e3570ef11e17ab1cf3b2f
natalieagus <nn88blue@gmail.com>
2015-03-23 13:18:27 +0800

    Initial Commit

---
+++ GraphicsProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphicsProject
+//
+//  Created by Natalie Agus on 23/3/15.
+//  Copyright (c) 2015 Natalie Agus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
